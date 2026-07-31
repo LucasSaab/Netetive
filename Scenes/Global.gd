d538b88df3b56a0cb6471ce5fd516ec454815a11
RodrigoEsR <rodrigoeschiarojas2@edu.unifil.br>
2026-07-31 20:23:05 -0300

    Adicionado funcoes para decrementar ou incrementar o dinheiro e incrementar a fama

--- Scenes/Global.gd
+++ Scenes/Global.gd
@@ -2,10 +2,8 @@ extends Node
 
 
 var fame: int = 0
-var money: double = 0
+var money: float = 0
 
-# Avisos sobre a alter~
-signal altered_money(new_money: int)
+# Avisos sobre a alterac~
+signal altered_money(new_money: float)
 signal fame_received(more_fame: int)
-
-func add_money
