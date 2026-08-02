3fb176c69531664215122e4b53cdc67e66caa8d2
RodrigoEsR <rodrigoeschiarojas2@edu.unifil.br>
2026-08-02 09:44:01 -0300

    Correcao da funcao drecrease money para decrease money

--- Scenes/Global.gd
+++ Scenes/Global.gd
@@ -10,20 +10,21 @@ signal fame_received(more_fame: int)
 
 # Funcao para incrementar o dinheiro
  
+# Funcao para incrementar o dinheiro
 func add_money(qtd: float) -> void:
 	money += qtd
-	print("Seu saldo atua ")
+	print("Seu saldo atual e de: %.2f" % money)
 	altered_money.emit(money) # Notifica a UI que o valor mudou
 
 # Funcao para incrementar a fama
-
 func add_fame(qtd: int) -> void:
 	fame += qtd
+	print("Sua fama atual e de: %d" % fame)
 	fame_received.emit(fame) # Notifica a UI que o valor mudou
 	
 # Funcao para decrementar o dinheiro recebido
-
-func drecrease_money(qtd: float) -> void:
+func decrease_money(qtd: float) -> void:
 	money -= qtd
+	print("Seu saldo atual e de: %.2f" % money)
 	altered_money.emit(money) # Notifica a UI que o valor mudou
 	
